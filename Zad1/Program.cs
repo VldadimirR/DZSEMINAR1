8282ed3bb5f36211215a17868c0ee143e8699c26
VladimirR <Vova.ru0007@yandex.ru>
2022-08-12 16:37:59 +0500

    1zad

---
+++ Zad1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int userNumberA = new int();
+int userNumberB = new int();
+Console.WriteLine("Введите 1  число");
+userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+userNumberB = Convert.ToInt32(Console.ReadLine());
+if(userNumberA > userNumberB)
+{
+    Console.WriteLine($"Наибольшее число {userNumberA}, наименьшее {userNumberB}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число {userNumberB}, наименьшее {userNumberA}");
+}
