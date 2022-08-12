4538716301f2477cdd24cddb31a741826395197e
VladimirR <Vova.ru0007@yandex.ru>
2022-08-12 17:01:20 +0500

    2zad

---
+++ Zad2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int userNumberA = new int();
+int userNumberB = new int();
+int userNumberC = new int();
+Console.WriteLine("Введите 1  число");
+userNumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+userNumberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число");
+userNumberC = Convert.ToInt32(Console.ReadLine());
+
+int max = userNumberA;
+if ( userNumberA > max) max = userNumberA;
+if ( userNumberB > max) max = userNumberB;
+if (userNumberC > max) max = userNumberC;
+Console.WriteLine($"Наибольшее число  - {max}");
